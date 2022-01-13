7afa3caa0844951006a8501f6a4be2143d4d47ab
Hanna-Syniavska <annarybalko34@gmail.com>
2022-01-13 18:11:49 +0200

    get items for autocomplete from graphql

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './styles/App.css';
-// import TextField from '@mui/material/TextField';
-// import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 
@@ -12,33 +12,113 @@ function App() {
   const [chars, setChars] = useState([]);
   const [pageCount, setPageCount] = useState(1);
   const [page, setPage] = useState(1);
+  const [inputValue, setInputValue] = useState('');
+  const [filteredChars, setFilteredChars] = useState([]);
+
+  // useEffect(() => {
+  //   const loadChars = async (page) => {
+  //     const response = await axios.get(`https://rickandmortyapi.com/api/character/?page=${page}`);
+  //     console.log(response.data);
+  //     setChars(response.data.results);
+  //     setPageCount(response.data.info.pages);
+  //   }
+  //   loadChars(page)
+  // }, [page])
+  
+  const query = `{
+    characters(page: ${page}) {
+      info {
+        pages
+      }
+      results {
+        id,
+        name,
+        status
+      }
+    }
+  }`
+
+  const filterQuery = `{
+    characters(filter: { name: "${inputValue}" }) {
+      results {
+        id,
+        name
+      }
+    }
+  }`
 
   useEffect(() => {
-    const loadChars = async (page) => {
-      const response = await axios.get(`https://rickandmortyapi.com/api/character/?page=${page}`);
-      console.log(response.data)
-      setChars(response.data.results)
-      setPageCount(response.data.info.pages)
+    const getFilteredChars = async () => {
+      await axios.post(`https://rickandmortyapi.com/graphql`, 
+      {
+        query: filterQuery,
+        variables: {}
+      }, {
+        headers: {"Content-Type": "application/json"},
+      },)
+      .then((response) => {
+        console.log('filtered' + response);
+        setFilteredChars(response.data.data.characters.results);  
+      })
+      .catch((error) => console.log(error))
     }
-    loadChars(page)
+
+    getFilteredChars();
+  }, [inputValue])
+
+  useEffect(() => {
+    const loadChars = async () => {
+      await axios.post(`https://rickandmortyapi.com/graphql`, 
+      {
+        query: query,
+        variables: {}
+      }, {
+        headers: {"Content-Type": "application/json"},
+      },)
+      .then((response) => {
+        console.log('allChars' + response);
+        setChars(response.data.data.characters.results);
+        setPageCount(response.data.data.characters.info.pages);     
+      })
+      .catch((error) => console.log(error))
+    }
+
+    loadChars();
   }, [page])
-  
+
+  const handleInputChange = (e, value) => {
+    setInputValue(value)
+  }
+
   const changePage = (event) => {
     setPage(parseInt(event.target.innerText));
   };
+
+  const openDetails = (e, value) => {
+    console.log(value);
+  }
 
   return (
     <div className="App">
       <div>
         <h1 className="title">Rick and Morty characters</h1>
-        {/* <Autocomplete
+        <Autocomplete
           disablePortal
-          id="combo-box-demo"
-          options={api.results}
+          id="autocomplete"
+          options={filteredChars}
+          renderOption={(props, option) => {
+            return (
+              <li {...props} key={option.id}>
+                {option.name}
+              </li>
+            );
+          }}
+          isOptionEqualToValue={(option, value) => option !== value}
           getOptionLabel={(option) => option.name}
-          sx={{ width: 300 }}
           renderInput={(params) => <TextField {...params} label="Choose character" />}
-        /> */}
+          onInputChange={handleInputChange}
+          onChange={openDetails}
+        />
         <ul className="list">
           {chars.map((char) =>
             <li
